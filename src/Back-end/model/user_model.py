6bf028a067060cad79079b2f290f0af6b926057c
Võ Cao Trí <trivo.131003@gmail.com>
2024-01-05 01:21:44 +0700

    Update user_model.py

--- src/Back-end/model/user_model.py
+++ src/Back-end/model/user_model.py
@@ -33,9 +33,12 @@ class user_model():
             self.cur.execute(f"CALL CreateAccount('{data['phoneNumber']}', '{password}', '{data['fullName']}', '{data['address']}', '{data['email']}', '{data['birthday']}', '{data['isCarOwner']}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 201)
+                #return make_response({"payload": result}, 201)
+                result = {"data":result, "status_code": 201}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 400)
+            result = {"data":str(e), "status_code": 400}
+            return json.dumps(result)
   
 
     #đăng nhập 
@@ -50,7 +53,9 @@ class user_model():
                 return json.dumps(result)
                 # return result
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 401)
+            result = {"data":str(e), "status_code": 400}
+            return json.dumps(result)
+            # return make_response({"message": "Failed: " + str(e)}, 401)
  
     # sửa thông tin xe
     def edit_car_model(self, data, carID):
@@ -58,18 +63,26 @@ class user_model():
             self.cur.execute(f"CALL editCar ('{carID}', '{data['carCompany']}', '{data['model']}', '{data['seats']}', '{data['transmission']}', '{data['transmission']}', '{data['fuelType']}', '{data['yearRelease']}', '{data['price']}'))")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 201)
+                #return make_response({"payload": result}, 201)
+                result = {"data":result, "status_code": 201}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 202)
+            # return make_response({"message": "Failed: " + str(e)}, 202)
+            result = {"data":str(e), "status_code": 202}
+            return json.dumps(result)
  
     def add_new_car_model(self, data):
         try:
             self.cur.execute(f"CALL addNewCar('{data['carCompany']}', '{data['model']}', '{data['seats']}', '{data['transmission']}', '{data['fuelType']}', '{data['yearRelease']}', '{data['price']}', '{data['phoneNumber']}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 201)
+                #return make_response({"payload": result}, 201)
+                result = {"data":result, "status_code": 201}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 400)
+            # return make_response({"message": "Failed: " + str(e)}, 400)
+            result = {"data":str(e), "status_code": 400}
+            return json.dumps(result)
 
     # xem thông tin xe 'oki'
     def get_car_model(self):
@@ -77,9 +90,13 @@ class user_model():
             self.cur.execute(f"Select * from Car")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            #return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
         
     # xem thông tin chi tiết của xe
     def get_detail_car_model(self, data):
@@ -87,9 +104,13 @@ class user_model():
             self.cur.execute(f"CALL GetCarInfo('{data['carID']}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            #return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
 
     #thêm thuê xe 
     def add_rental_model(self, data, cusID, carID):
@@ -97,10 +118,13 @@ class user_model():
             self.cur.execute(f"CALL InsertRental('{cusID}', '{carID}', '{data['pickupTime']}', '{data['returnTime']}', '{data['rentalPrice']}', '{data['retalLocationID']}', '{data['paymentID']}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 201)
+                #return make_response({"payload": result}, 201)
+                result = {"data":result, "status_code": 201}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 400)
-        
+            #return make_response({"message": "Failed: " + str(e)}, 400)
+            result = {"data":str(e), "status_code": 400}
+            return json.dumps(result)
 
     #thêm thanh toán
     def add_payment_model(self, data):
@@ -108,9 +132,13 @@ class user_model():
             self.cur.execute(f"CALL AddPayment('{data['carID']}', '{data['paymentMethod']}', '{data['paymentDate']}', '{data['discountCode']}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 201)
+                #return make_response({"payload": result}, 201)
+                result = {"data":result, "status_code": 201}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 400)
+            #return make_response({"message": "Failed: " + str(e)}, 400)
+            result = {"data":str(e), "status_code": 400}
+            return json.dumps(result)
     
     # xem thông tin chuyến đi hiện tại
     def current_trip_model(self, cusID):
@@ -118,18 +146,26 @@ class user_model():
             self.cur.execute(f"CALL GetCurrentTrip('{cusID}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            # return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
 
     def current_rental_trip_list_model(self, cusID):
         try:    
             self.cur.execute(f"CALL GetCurrentRentalCarList('{cusID}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            # return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
         
     # xem lịch sử thuê xe
     def rental_history_model(self, cusID):
@@ -137,18 +173,26 @@ class user_model():
             self.cur.execute(f"CALL GetRentalHistory('{cusID}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            #return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
 
     def trip_history_model(self, cusID):
         try:
             self.cur.execute(f"CALL GetTripHistory('{cusID}')")
             result = self.cur.fetchall()
             if len(result)>0:
-                return make_response({"payload": result}, 200)
+                #return make_response({"payload": result}, 200)
+                result = {"data":result, "status_code": 200}
+                return json.dumps(result)
         except Exception as e:
-            return make_response({"message": "Failed: " + str(e)}, 204)
+            # return make_response({"message": "Failed: " + str(e)}, 204)
+            result = {"data":str(e), "status_code": 204}
+            return json.dumps(result)
 
 
     # def delete_car_model(self, id):
