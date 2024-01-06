4937cfb07f8d442feaa7856f946d4bc95f52f504
Võ Cao Trí <trivo.131003@gmail.com>
2024-01-06 14:25:23 +0700

    Update account_model.dart

--- vivu/lib/models/account_model.dart
+++ vivu/lib/models/account_model.dart
@@ -1,37 +1,45 @@
-class AccountModel {
-  AccountModel({
-    this.phoneNumber,
-    //this.passwordAccount,
-    this.fullName,
-    this.address,
-    this.email,
-    this.birthday,
-    this.isCarOwner,
-    this.id,
-  }) {
-    phoneNumber = "";
-    //passwordAccount = "";
-    fullName = "";
-    address = "";
-    email = "";
-    birthday = "";
-    isCarOwner = "";
-    id = "";
+class Account {
+  String accountID;
+  String address;
+  String birthday;
+  String email;
+  String fullName;
+  int isCarOwner;
+  String phoneNumber;
+
+  Account({
+  this.accountID = "",
+  this.address = "",
+  this.birthday = "",
+  this.email = "",
+  this.fullName = "",
+  this.isCarOwner = 0,
+  this.phoneNumber = "",
+});
+
+  // Factory method to create an Account instance from a Map
+  factory Account.fromMap(Map<String, dynamic> map) {
+    return Account(
+      accountID: map['accountID'],
+      address: map['address'],
+      birthday: map['birthday'],
+      email: map['email'],
+      fullName: map['fullName'],
+      isCarOwner: map['isCarOwner'],
+      phoneNumber: map['phoneNumber'],
+    );
   }
 
-  String? phoneNumber;
-
-  //String? passwordAccount;
-
-  String? fullName;
-
-  String? address;
-
-  String? email;
-
-  String? birthday;
-
-  String? isCarOwner;
-
-  String? id;
+  // Method to convert Account instance to a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'accountID': accountID,
+      'address': address,
+      'birthday': birthday,
+      'email': email,
+      'fullName': fullName,
+      'isCarOwner': isCarOwner,
+      'phoneNumber': phoneNumber,
+    };
+  }
 }
